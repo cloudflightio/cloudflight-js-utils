436431d08ee02fd56c7e708b55db398b1bf3aadd
Clemens Grabmann <clemens.grabmann@cloudflight.io>
2022-09-02 10:32:23 +0200

    add skip operator

---
+++ packages/akita-read/src/lib/operators/skip.operator.ts
@@ -0,0 +1,10 @@
+import { MaybeCancellingPipeOperator } from '../util/pipe.operator';
+import { skip as RxSkip } from 'rxjs/operators';
+import { identityValueOperator } from './identity-value-operator.util';
+
+export function skip<I>(count: number): MaybeCancellingPipeOperator<I, I> {
+    return {
+        observableOperator: RxSkip(count),
+        valueOperator: identityValueOperator,
+    };
+}
