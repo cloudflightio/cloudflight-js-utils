5dbbbe39387d50e0f5726dbe0feaee0518fbe139
strawberry-choco <kangrui.ye@cloudflight.io>
2022-09-26 09:04:39 +0200

    Fix publish (#12)
    * add missing doc for generated css
    
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>
    
    * make packages publish ready
    
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>
    
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -9,7 +9,6 @@
 
 import { readCachedProjectGraph } from '@nrwl/devkit';
 import { execSync } from 'child_process';
-import { readFileSync, writeFileSync } from 'fs';
 import chalk from 'chalk';
 
 function invariant(condition, message) {
@@ -21,14 +20,7 @@ function invariant(condition, message) {
 
 // Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
 // Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const [, , name, version, tag = 'next'] = process.argv;
-
-// A simple SemVer validation to validate the version
-const validVersion = /^\d+\.\d+\.\d(-\w+\.\d+)?/;
-invariant(
-  version && validVersion.test(version),
-  `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
-);
+const [, , name] = process.argv;
 
 const graph = readCachedProjectGraph();
 const project = graph.nodes[name];
@@ -38,7 +30,10 @@ invariant(
   `Could not find project "${name}" in the workspace. Is the project.json configured correctly?`
 );
 
-const outputPath = project.data?.targets?.build?.options?.outputPath;
+const outputPath =
+  project.data?.targets?.build?.options?.outputPath ??
+  project.data?.targets?.build?.outputs?.[0];
+
 invariant(
   outputPath,
   `Could not find "build.options.outputPath" of project "${name}". Is project.json configured  correctly?`
@@ -46,16 +41,5 @@ invariant(
 
 process.chdir(outputPath);
 
-// Updating the version in "package.json" before publishing
-try {
-  const json = JSON.parse(readFileSync(`package.json`).toString());
-  json.version = version;
-  writeFileSync(`package.json`, JSON.stringify(json, null, 2));
-} catch (e) {
-  console.error(
-    chalk.bold.red(`Error reading package.json file from library build output.`)
-  );
-}
-
 // Execute "yarn publish" to publish
-execSync(`yarn publish --access public --tag ${tag}`);
+execSync(`yarn publish`);
