98f0e7d832c4b35fb92890ba86c9d1b17965cfe9
Kangrui Ye <kangrui.ye@cloudflight.io>
2022-10-03 22:31:39 +0200

    fix doc config for github pages
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- apps/documentation/src/.vuepress/config.ts
+++ apps/documentation/src/.vuepress/config.ts
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress';
 
 export default defineUserConfig({
   title: 'Cloudflight Js Utils',
+  base: '/cloudflight-js-utils/',
   dest: 'dist/apps/documentation',
   bundler: webpackBundler({
     postcss: {},
